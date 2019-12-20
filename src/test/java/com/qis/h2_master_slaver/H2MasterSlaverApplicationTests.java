5298ec1f6b86794c21a6e233deb3e314bc9b2d28
QiS1026 <1026310966@qq.com>
2019-12-20 09:46:30 +0800

    api test

--- src/test/java/com/qis/h2_master_slaver/H2MasterSlaverApplicationTests.java
+++ src/test/java/com/qis/h2_master_slaver/H2MasterSlaverApplicationTests.java
@@ -1,13 +1,25 @@
 package com.qis.h2_master_slaver;
 
+import com.qis.h2_master_slaver.controller.PersonController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class H2MasterSlaverApplicationTests {
 
+    @Autowired
+    private PersonController personController;
+
     @Test
     void contextLoads() {
+        personController.masterTableCreate();
+        personController.masterInsert(1, "张三");
+        System.out.println(personController.masterSelectAll());
+
+        personController.slaverTableCreate();
+        personController.slaverInsert(1, "李四");
+        System.out.println(personController.slaverSelectAll());
     }
 
 }
